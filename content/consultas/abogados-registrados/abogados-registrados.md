6bab244e7bb75226ca8461c36be6737aaf50516b
Guillermo Valdés <guillermo@movimientolibre.com>
2021-02-09 14:20:24 -0600

    Abogados registrados API en pruebas.

--- content/consultas/abogados-registrados/abogados-registrados.md
+++ content/consultas/abogados-registrados/abogados-registrados.md
@@ -8,13 +8,66 @@ Modified: 2021-02-08 20:00:00
 JavaScripts: consultas-abogados-registrados.js
 
 
-<table id="abogadosRegistrados" class="table" style="width:100%">
-<thead>
-<th>libro</th>
-<th>nombre</th>
-<th>fecha</th>
-<th>estatus</th>
-<th>numero</th>
-<th>id</th>
-</thead>
-</table>
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="../">Consultas</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Abogados registrados</li>
+    </ol>
+</nav>
+
+## Instrucciones
+
+1. Seleccione el año de registro.
+2. Opcionalmente puede escribir parte del nombre.
+3. Presione el botón **Consultar** y espere a que se carguen los abogados registrados. Tiene un límite de 100 resultados.
+4. Puede usar el campo **Filtrar** para encontrar algo específico en los resultados.
+
+<div class="card mb-2">
+    <div class="card-body">
+        <form id="abogadosForm">
+            <div class="form-group">
+                <label for="anoSelect">Año de registro</label>
+                <select id="anoSelect" class="form-control">
+                    <option>2021</option>
+                    <option>2020</option>
+                    <option>2019</option>
+                    <option>2018</option>
+                    <option>2017</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="nombreInput">Nombre</label>
+                <input id="nombreInput" type="text" class="form-control" aria-describedby="nombreInputHelp">
+                <small id="nombreInputHelp" class="form-text text-muted">No use acentos, eñes y caracteres especiales.</small>
+            </div>
+            <button id="consultarButton" type="button" class="btn btn-primary">
+                Consultar
+            </button>
+            <button id="cargandoButton" class="btn btn-primary" type="button"  style="display: none;" disabled>
+                <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                Cargando...
+            </button>
+        </form>
+    </div>
+</div>
+<div id="sinResultados" class="card mb-2" style="display: none;">
+    <div class="card-body">
+        <div id="sinResultadosAlert" class="alert alert-warning" role="alert">
+            No se encontraron abogados registrados con las opciones dadas.
+        </div>
+    </div>
+</div>
+<div id="abogadosRegistrados" class="card mb-2" style="display: none;">
+    <div class="card-body">
+        <table id="abogadosRegistradosTable" class="table" style="width: 100%;">
+            <thead>
+                <th>libro</th>
+                <th>nombre</th>
+                <th>fecha</th>
+                <th>estatus</th>
+                <th>numero</th>
+                <th>id</th>
+            </thead>
+        </table>
+    </div>
+</div>
--- content/js/consultas-abogados-registrados.js
+++ content/js/consultas-abogados-registrados.js
@@ -1,18 +1,56 @@
 // Consultas Abogados Registrados
 $(document).ready(function () {
 
-    $.ajax({
-        'url': "http://127.0.0.1:8000/abogados?ano=2019",
-        'type': "GET",
-        'dataType': "json",
-        'success': function (data) {
-            var mapeado = data.map(el => Object.values(el));
-            alCargarConExito(mapeado);
-        }
-    });
+    const ABOGADOS_PLATAFORMA_WEB_API_URL = "http://127.0.0.1:8000/abogados"
 
-    function alCargarConExito(mapeado) {
-        $('#abogadosRegistrados').DataTable({
+    // Al dar clic en el botón mostrar
+    $('#consultarButton').click(function () {
+
+        // Ocultar botón Consultar, mostrar botón Cargando... y ocultar mensaje Sin resultados
+        $('#consultarButton').hide();
+        $('#cargandoButton').show();
+        $('#sinResultados').hide();
+
+        // Llamar a la API y ejecutar acciones al recibir resultados
+        $.ajax({
+            'url': ABOGADOS_PLATAFORMA_WEB_API_URL,
+            'type': "GET",
+            'data': {
+                'ano': $('#anoSelect').val(),
+                'nombre': $('#nombreInput').val(),
+            },
+            'dataType': "json",
+            'success': function (data) {
+                var mapeado = data.map(el => Object.values(el));
+                alRecibirResultados(mapeado);
+            }
+        });
+
+    }); // Al dar clic en el botón mostrar
+
+    // Al recibir los datos de la API
+    function alRecibirResultados(mapeado) {
+
+        // Si no hay resultados, muestra mensaje y termina
+        if (mapeado.length == 0) {
+            $('#cargandoButton').hide();
+            $('#consultarButton').show();
+            $('#sinResultados').show();
+            $('#abogadosRegistrados').hide();
+            return;
+        };
+
+        // Si tiene datos, limpiar la tabla
+        if ($('#abogadosRegistradosTable').DataTable().data().length > 0) {
+            $('#abogadosRegistradosTable').DataTable().clear();
+            $('#abogadosRegistradosTable').DataTable().destroy();
+        };
+
+        // Mostrar tabla
+        $('#abogadosRegistrados').show();
+
+        // DataTable
+        $('#abogadosRegistradosTable').DataTable({
             'data': mapeado,
             'columns': [
                 { 'title': "Libro" },
@@ -28,7 +66,7 @@ $(document).ready(function () {
                     'visible': false
                 }
             ],
-            'pageLength': 50,
+            'pageLength': 10,
             'language': {
                 'lengthMenu': "Mostrar _MENU_",
                 'search': "Filtrar:",
@@ -44,6 +82,11 @@ $(document).ready(function () {
                 }
             }
         });
-    };
+
+        // Mostrar botón Consultar y ocultar botón Cargando...
+        $('#consultarButton').show();
+        $('#cargandoButton').hide();
+
+    }; // Al recibir los datos de la API
 
 });
