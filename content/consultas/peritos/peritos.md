9236b72d45423cd6abc1461066b230afa72ceb26
anamariamr <63805557+anamariamr@users.noreply.github.com>
2021-02-22 13:55:24 -0600

    Se agrega Peritos a consultas

---
+++ content/consultas/peritos/peritos.md
@@ -0,0 +1,64 @@
+Title: Lista de Peritos
+Summary: La lista de Peritos en los términos de la Ley Orgánica del Poder Judicial del Estado.
+Slug: consultas-peritos
+URL: consultas/peritos/
+Save_As: consultas/peritos/index.html
+Date: 2021-02-22 13:00:00
+Modified: 2021-02-22 13:00:00
+JavaScripts: consultas-peritos.js
+
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="../">Consultas</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Peritos</li>
+    </ol>
+</nav>
+
+## Instrucciones
+
+<div class="card mb-2">
+    <div class="card-body">
+        <form id="peritosForm">
+            <div class="form-group">
+                <label for="distritoSelect">Distrito:</label>
+                <select id="distritoSelect" class="form-control"></select>
+            </div>
+            <div class="form-group">
+                <label for="nombreInput">Nombre:</label>
+                <input id="nombreInput" type="text" class="form-control" aria-describedby="nombreInputHelp">
+                <small id="nombreInputHelp" class="form-text text-muted">No use acentos, eñes, diéresis o caracteres especiales.</small>
+            </div>
+            <button id="consultarButton" type="button" class="btn btn-primary">
+                Consultar
+            </button>
+            <button id="cargandoButton" class="btn btn-primary" type="button"  style="display: none;" disabled>
+                <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                Cargando...
+            </button>
+        </form>
+    </div>
+</div>
+
+<div id="revisarParametros" class="card mb-2" style="display: none;">
+    <div class="card-body">
+        <div id="revisarParametrosAlert" class="alert alert-primary" role="alert"></div>
+    </div>
+</div>
+<div id="sinResultados" class="card mb-2" style="display: none;">
+    <div class="card-body">
+        <div id="sinResultadosAlert" class="alert alert-warning" role="alert"></div>
+    </div>
+</div>
+<div id="peritosRegistrados" class="card mb-2" style="display: none;">
+    <div class="card-body">
+        <table id="peritosRegistradosTable" class="table" style="width: 100%;">
+            <thead>
+                <tr>
+                    <th>Distrito</th>
+                    <th>Tipo</th>
+                    <th>Nombre</th>
+                </tr>
+            </thead>
+        </table>
+    </div>
+</div>
---
+++ content/js/consultas-peritos.js
@@ -0,0 +1,123 @@
+// Consultas Peritos
+$(document).ready(function() {
+
+    // Para desarrollo
+    //const DISTRITOS_PLATAFORMA_WEB_API_URL = "http://localhost:8000/distritos"
+    //const PERITOS_PLATAFORMA_WEB_API_URL = "http://localhost:8000/peritos"
+
+
+    // Para producción
+    //const DISTRITOS_PLATAFORMA_WEB_API_URL = "https://plataforma-web-api-dot-pjecz-268521.uc.r.appspot.com/distritos"
+    //const PERITOS_PLATAFORMA_WEB_API_URL = "https://plataforma-web-api-dot-pjecz-268521.uc.r.appspot.com/peritos"
+
+    // Llamar a la API de Distritos para alimentar distritoSelect
+    $.ajax({
+        'url': DISTRITOS_PLATAFORMA_WEB_API_URL,
+        'type': "GET",
+        'dataType': "json",
+        'success': function(dataDistritos) {
+            alRecibirDistritos(dataDistritos);
+        }
+    });
+
+    function alRecibirDistritos(dataDistritos) {
+        $.each(dataDistritos, function(i, distrito) {
+            $('#distritoSelect').append($('<option>', {
+                value: distrito.id,
+                text: distrito.nombre
+            }));
+        });
+    };
+
+
+    // Al dar clic en el botón Consultar
+    $('#consultarButton').click(function() {
+
+        // Validar
+        var valido = true;
+        if ($('#nombreInput').val().trim() == '') {
+            $('#revisarParametrosAlert').text("Falta el nombre.");
+            valido = false;
+        };
+
+        // Si es válido el formulario
+        if (valido) {
+            // Ocultar botón Consultar, mostrar botón Cargando... y ocultar mensaje Sin resultados
+            $('#consultarButton').hide();
+            $('#cargandoButton').show();
+            $('#revisarParametros').hide();
+            $('#sinResultados').hide();
+            // Llamar a la API y ejecutar acciones hasta recibir resultados
+            $.ajax({
+                'url': PERITOS_PLATAFORMA_WEB_API_URL,
+                'type': "GET",
+                'data': {
+                    'distrito': $('#autoridadSelect').val(),
+                    'nombre': $('#nombreInput').val().trim()
+                },
+                'dataType': "json",
+                'success': function(data) {
+                    alRecibirResultados(data);
+                }
+            });
+        } else {
+            $('#revisarParametros').show();
+            $('#peritosRegistrados').hide();
+        };
+
+    });
+
+    // Al recibir los datos de la API
+    function alRecibirResultados(data) {
+
+        // Si tiene datos, limpiar la tabla
+        if ($('#peritosRegistradosTable').length > 0) {
+            $('#peritosRegistradosTable').DataTable().clear();
+            $('#peritosRegistradosTable').DataTable().destroy();
+        };
+
+        // Si no hay resultados, muestra mensaje y termina
+        if (data.length == 0) {
+            $('#cargandoButton').hide();
+            $('#consultarButton').show();
+            $('#sinResultados').show();
+            $('#sinResultadosAlert').text("No se encontraron expedientes registrados con las opciones dadas.");
+            $('#peritosRegistrados').hide();
+            return;
+        };
+
+        // Mostrar tabla
+        $('#peritosRegistrados').show();
+
+        // DataTable
+        $('#peritosRegistradosTable').DataTable({
+            'data': data,
+            'columns': [
+                { 'data': "distrito", 'width': "30%" },
+                { 'data': "tipo", 'width': "30%" },
+                { 'data': "nombre", 'width': "40%" }
+            ],
+            'pageLength': 10,
+            'language': {
+                'lengthMenu': "Mostrar _MENU_",
+                'search': "Filtrar:",
+                'zeroRecords': "Cargando información...",
+                'info': "Página _PAGE_ de _PAGES_",
+                'infoEmpty': "No hay registros",
+                'infoFiltered': "(filtrados desde _MAX_ registros totales)",
+                'oPaginate': {
+                    'sFirst': "Primero",
+                    'sLast': "Último",
+                    'sNext': "Siguiente",
+                    'sPrevious': "Anterior"
+                }
+            }
+        });
+
+        // Mostrar botón Consultar y ocultar botón Cargando...
+        $('#consultarButton').show();
+        $('#cargandoButton').hide();
+
+    }; // Al recibir los datos de la API
+
+});
